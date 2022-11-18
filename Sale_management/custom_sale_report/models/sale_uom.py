76d5587633a88d62e2bcc4690a818f91c648a951
ATCHE Maurice <maurice.atche@halltech-africa.com>
2022-11-18 10:56:24 +0000

    Adding sale_uom in custom_sale_report

--- Sale_management/custom_sale_report/models/sale_uom.py
+++ Sale_management/custom_sale_report/models/sale_uom.py
@@ -6,7 +6,10 @@ from odoo import models, fields, api
 class SaleUom(models.Model):
     _name = "sale.uom"
     _description = "Unite de mesure de vente"
-    
-    
+        
     name = fields.Char(string="Nom")
     code = fields.Char(string="Symbole")
+    
+    def create(vals):
+        result = super(SaleUom, self).create(vals)
+        return result
